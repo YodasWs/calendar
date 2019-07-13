7059b37ae0dff9e8203905ca037f15fe18d2a29a
Sam Grundman <samuel@yodas.ws>
2019-07-13 12:58:45 -0400

    Return to a more viewer-friendly scale

--- src/webgl.js
+++ src/webgl.js
@@ -118,15 +118,12 @@ function loadShader(gl, type, source) {
 	const shader = gl.createShader(type);
 
 	// Send the source to the shader object
-
 	gl.shaderSource(shader, source);
 
 	// Compile the shader program
-
 	gl.compileShader(shader);
 
 	// See if it compiled successfully
-
 	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
 		console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
 		gl.deleteShader(shader);
